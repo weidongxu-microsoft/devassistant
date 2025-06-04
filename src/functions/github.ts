eb4ffbd874cf55c659103451d4ce03000b1da3c1
Weidong Xu <weidxu@microsoft.com>
2025-06-04 14:19:16 +0800

    update

--- src/functions/github.ts
+++ src/functions/github.ts
@@ -4,6 +4,7 @@ interface Response extends HttpResponseInit {
   status: number;
   jsonBody: {
     result: string;
+    details: string;
   };
 }
 
@@ -16,6 +17,7 @@ export async function githubPullRequest(
     status: 200,
     jsonBody: {
       result: "",
+      details: "",
     },
   };
 
@@ -32,24 +34,30 @@ export async function githubPullRequest(
           const repo = match[2];
           const pullNumber = match[3];
 
-          const result = [];
+          const failedChecks = [];
+          let details = "";
 
           const prResponse = await fetch("https://api.github.com/repos/" + owner + "/" + repo + "/pulls/" + pullNumber);
           const headSha = (await prResponse.json()).head.sha;
 
-          const checksResponse = await fetch("https://api.github.com/repos/" + owner + "/" + repo + "/commits/" + headSha + "/check-runs");
+          const checksResponse = await fetch("https://api.github.com/repos/" + owner + "/" + repo + "/commits/" + headSha + "/check-runs?per_page=100");
           const checkRuns = (await checksResponse.json()).check_runs;
           for (const checkRun of checkRuns) {
-            if (checkRun.conclusion === "failure") {
-              result.push("Check failed: " + checkRun.name);
+            if (checkRun.conclusion === "failure" && !checkRun.name.includes("[TEST-IGNORE]")) {
+              if (checkRun.name.includes("azure-sdk-for-java")) {
+                const javaResponse = await fetch("https://api.github.com/repos/" + owner + "/" + repo + "/check-runs/" + checkRun.id);
+                const javaCheckRun = await javaResponse.json();
+                details = javaCheckRun.output.text.split("\n").filter(it => it.includes("[Java-Sdk-Automation")).map(it => it.replace("cmderr", "").trim()).join("\n");
+              }
+
+              failedChecks.push({
+                name: checkRun.name,
+              });
             }
           }
 
-          if (result.length === 0) {
-            res.jsonBody.result = "All checks passed.";
-          } else {
-            res.jsonBody.result = result.join("\n");
-          }
+          res.jsonBody.result = failedChecks.length === 0 ? "Success" : "Failed";
+          res.jsonBody.details = details;
         }
 
         break;
